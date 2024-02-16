e8ba8bcd2ff0555a1d066e8c4134b37fdca5e5a0
Alireza Ghiasy-L <sina121gh@gmail.com>
2024-02-16 11:00:01 +0330

    added role model

---
+++ TopLearn.DataLayer/Entities/User/Role.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopLearn.DataLayer.Entities.User
+{
+    public class Role
+    {
+        [Key]
+        public int RoleId { get; set; }
+
+        [Display(Name = "")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(50, ErrorMessage = "{0} نمیتواند بیشتر از {1} کاراکتر باشد")]
+        public string? RoleTitle { get; set; }
+    }
+}
