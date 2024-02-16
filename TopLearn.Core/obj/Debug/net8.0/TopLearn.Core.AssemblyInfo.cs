f3186c857761e9ff299474a12d02bb7f0352cd1e
Alireza Ghiasy-L <sina121gh@gmail.com>
2024-02-16 16:52:37 +0330

    added template to project

--- TopLearn.Core/obj/Debug/net8.0/TopLearn.Core.AssemblyInfo.cs
+++ TopLearn.Core/obj/Debug/net8.0/TopLearn.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TopLearn.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+880ccdbf6f913783cb1aa9025f52962143321eac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41a7db7418316b4d42e967df824a342b097d6b0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TopLearn.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TopLearn.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- TopLearn.Web/Controllers/HomeController.cs
+++ TopLearn.Web/Controllers/HomeController.cs
@@ -4,9 +4,6 @@ namespace TopLearn.Web.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
-        {
-            return Content("Hello");
-        }
+        public IActionResult Index() => View();
     }
 }
