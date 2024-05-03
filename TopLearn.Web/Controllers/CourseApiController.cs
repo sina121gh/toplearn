bd47b7d41ab4aafdf8527a9f4301c72b59d0448b
Alireza Ghiasy <sina121gh@gmail.com>
2024-05-03 19:24:22 +0330

    added jquery quto complete for search courses in landing page

---
+++ TopLearn.Web/Controllers/CourseApiController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TopLearn.DataLayer.Context;
+
+namespace TopLearn.Web.Controllers
+{
+    [Route("api/course")]
+    [ApiController]
+    public class CourseApiController : ControllerBase
+    {
+        private readonly TopLearnContext _context;
+
+        public CourseApiController(TopLearnContext context)
+        {
+            _context = context;
+        }
+
+        [Produces("application/json")]
+        [HttpGet("search")]
+        public async Task<IActionResult> Search()
+        {
+            try
+            {
+                string term = HttpContext.Request.Query["term"].ToString();
+                var courseTitles = await _context.Courses
+                    .Where(c => c.Title.Contains(term))
+                    .Select(c => c.Title).ToListAsync();
+
+                return Ok(courseTitles);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
--- TopLearn.Web/obj/Debug/net8.0/TopLearn.Web.AssemblyInfo.cs
+++ TopLearn.Web/obj/Debug/net8.0/TopLearn.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TopLearn.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d45a4a31124993d6742e3ae08224fe55fcf46dcb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ab53b24520b3b25c5a118fb3c8734ae7fee1415")]
 [assembly: System.Reflection.AssemblyProductAttribute("TopLearn.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TopLearn.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
